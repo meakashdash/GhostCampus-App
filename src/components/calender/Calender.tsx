cc5c0cb7d70666661ed8e6b123649f0ef1724749
meakashdash1 <akashdash279@gmail.com>
2024-06-24 23:27:17 +0530

    basic calender implemented

--- src/components/calender/Calender.tsx
+++ src/components/calender/Calender.tsx
@@ -3,10 +3,35 @@ import { SafeAreaView, StyleSheet, View } from 'react-native';
 import DateCell from './DateCell';
 
 const Calender = ():React.JSX.Element => {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    let dayone = new Date(year, month, 1).getDay();
+    let lastdate = new Date(year, month + 1, 0).getDate();
+    let dayend = new Date(year, month, lastdate).getDay();
+    let lastMonthDate = new Date(year, month, 0).getDate();
+    console.log("dayone",dayone);
+    console.log("lastdate",lastdate);
+    console.log("dayend",dayend);
+    console.log("lastMonthDate",lastMonthDate);
     return (
         <SafeAreaView>
             <DateCell
-                day={"4"}
+              day={"4"}
             />
         </SafeAreaView>
     );
