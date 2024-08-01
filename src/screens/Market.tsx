0ea2104c157efb0654028146294c9bc9a6ba2bca
meakashdash <akashdash279@gmail.com>
2024-08-02 01:18:30 +0530

    Enhancement: Market Card and Get all card implemented

--- src/screens/Market.tsx
+++ src/screens/Market.tsx
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {ActivityIndicator, FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Picker} from '@react-native-picker/picker';
 import {useRecoilState} from 'recoil';
 import {tokenState} from '../context/userContext';
 import axios from 'axios';
 import {baseUrl} from '../URL';
-import Search from '../../assets/icons/market/Search';
+import ItemCard from '../components/market/ItemCard';
+import moment from 'moment';
 
 interface Category {
   _id: string;
@@ -15,14 +16,48 @@ interface Category {
   childrens: string[];
 }
 
+interface Item {
+  _id: string;
+  image: string;
+  title: string;
+  categoryName: string;
+  price: number;
+  createdAt: string;
+  isLiked: boolean;
+}
+
 export const Market = () => {
   const [parentCategories, setParentCategories] = useState<Category[]>([]);
   const [token, setToken] = useRecoilState(tokenState);
   const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [items, setItems] = useState<Item[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getItems();
+  }, [currentPage]);
 
   useEffect(() => {
     getCategories();
   }, []);
+
+  const getItems=async()=>{
+    try {
+      setLoading(true);
+      const response=await axios.get(`${baseUrl}/market?page=${currentPage}&limit=20`,{
+        headers:{
+          Authorization:token
+        }
+      })
+      const newItems=response?.data?.items;
+      setItems(prevItems => [...prevItems, ...newItems]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 
   const getCategories = async () => {
     try {
@@ -38,6 +73,29 @@ export const Market = () => {
       throw error;
     }
   };
+
+  const renderLoader = () => {
+    return loading ? (
+      <View style={styles.loaderStyle}>
+        <ActivityIndicator size="large" color="#B20000" />
+      </View>
+    ) : null;
+  };
+
+  const loadMoreItem = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const renderItem=({item}: {item: Item})=>(
+    <ItemCard 
+      image={item.image?item.image:''}
+      title={item.title}
+      category={item.categoryName}
+      price={item.price}
+      date={moment(item.createdAt).format('DD MMM YY')}
+      isLiked={true}
+    />
+  )
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.pickerRow}>
@@ -78,6 +136,16 @@ export const Market = () => {
           </Picker>
         </View>
       </View>
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={(item) => item._id}
+        numColumns={2}
+        contentContainerStyle={styles.list}
+        ListFooterComponent={renderLoader}
+        onEndReached={loadMoreItem}
+        onEndReachedThreshold={0.5}
+      />
     </SafeAreaView>
   );
 };
@@ -86,15 +154,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000000',
-    padding: 12,
+    padding: 9,
   },
   pickerRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginBottom:10
   },
   pickerWrapper: {
     flex: 1,
-    marginHorizontal: 9,
+    marginHorizontal: 4,
     borderWidth: 1,
     borderColor: '#f4f4f4',
     borderRadius: 15,
@@ -104,5 +173,12 @@ const styles = StyleSheet.create({
     borderColor: '#f4f4f4',
     borderRadius: 15,
     color: 'white',
+  },
+  list: {
+    paddingBottom: 10,
+  },
+  loaderStyle: {
+    marginVertical: 16,
+    alignItems: 'center',
   },
 });
